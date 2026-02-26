10a2c37120a027e529f07e64288a9761bbc55ded
Yacinewhatchandcode <yacine.benhamou@gmail.com>
2026-02-26 13:45:18 +0100

    fix(build): Fix malformed JSON-LD syntax in HTML and setup vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), createHtmlPlugin({ minify: true })],
   server: {
     port: 3002,
     strictPort: true,
